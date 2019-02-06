b097adb9ca79f9d70226e851edd86591e9c122f4
William Bradshaw <wbradshaw4@my.bcit.ca>
2019-02-06 13:38:45 -0800

    Added two basic resources routes for Trainer and Client

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -74,6 +74,10 @@ $routes->setAutoRoute(true);
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
 
+$routes->add('trainer', 'Trainer');
+$routes->add('client', 'Client');
+
+
 /**
  * --------------------------------------------------------------------
  * Additional Routing
